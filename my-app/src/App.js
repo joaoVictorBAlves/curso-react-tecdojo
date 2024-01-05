784b05e110917b5ba8bee22d439ba39944bef991
João Victor <joaovba2002@alu.ufc.br>
2024-01-05 15:52:10 -0300

    aula 04

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
+      <h1>Curso de react</h1>
+      <div className='Components'>
+        <div className='Component'>
+          <h5>Componente #1</h5>
+          <HelloWorld />
+        </div> 
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/HelloWorld/index.jsx
@@ -0,0 +1,10 @@
+const HelloWorld = () => {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <p>That's my first component</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
