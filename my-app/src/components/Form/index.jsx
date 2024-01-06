92e478d3a430592131aad77b20b53eaa7c3cbcc0
João Victor <joaovba2002@alu.ufc.br>
2024-01-06 15:11:01 -0300

    aula-06

---
+++ my-app/src/components/Form/index.jsx
@@ -0,0 +1,17 @@
+const Form = () => {
+  return (
+    <div>
+      <h3>Formulário</h3>
+      <form>
+        <input type="text" placeholder="Escreva seu nome" />
+        <br />
+        <input type="number" placeholder="Digite sua idade" />
+        <br />
+        <br />
+        <button>Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ my-app/src/components/Lista/index.jsx
@@ -0,0 +1,15 @@
+const Lista = () => {
+  return (
+    <div>
+      <h3>Lista</h3>
+      <ul>
+        <li>Ítem 1</li>
+        <li>Ítem 2</li>
+        <li>Ítem 3</li>
+        <li>Ítem 4</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Lista;
