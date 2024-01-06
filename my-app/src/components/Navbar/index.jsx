7caf617fa5a29aaa3ee0eced040382d6e28c5355
João Victor <joaovba2002@alu.ufc.br>
2024-01-06 15:00:50 -0300

    usando o navbar

---
+++ my-app/src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div class="navbar">
+      <a href="#">Página Principal</a>
+      <a href="#">Sobre</a>
+      <a href="#">Serviços</a>
+      <a href="#">Contato</a>
+    </div>
+  );
+};
+
+export default Navbar;
