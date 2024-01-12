014ba9d34edcf4c283a24dca6447d03ac9ff6ab0
João Victor <joaovba2002@alu.ufc.br>
2024-01-11 23:29:23 -0300

    aula13

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,9 @@
 import Product from "./Product";
 
+function generateRandomValue(a, b) {
+  return Math.random() * (b - a) + a;
+}
+
 function App() {
   const frutas = ["Banana", "Maçã", "Limão", "Pera"];
 
@@ -30,13 +34,24 @@ function App() {
       id: 2,
       imgPath:
         "https://down-br.img.susercontent.com/file/br-11134207-7qukw-libflcic5ink3b",
-      name: "Nord Stage 3 2",
+      name: "Nord Stage 3",
       price: "$20009.99",
     },
   ];
 
+  // Pode ser renderizado ou não
+  const products2 = null;
+
+  const erro = 1;
+
+  // Exemplo de uso
+  var valorAleatorio = generateRandomValue(10, 20);
+  console.log(valorAleatorio);
+
   return (
     <div className="App">
+      <h1>Listagens</h1>
+
       <h3>Lista de Frutas</h3>
 
       <ul>
@@ -74,6 +89,21 @@ function App() {
           price={product.price}
         />
       ))}
+
+      <h1>Renderização Condicional</h1>
+      <h3>Alert de erro</h3>
+      {erro === 1 && <span className="Erro">Ocorreu um erro</span>}
+
+      <h3>Incerteza na Iteração</h3>
+      {products2 &&
+        products2.map((product) => (
+          <Product
+            key={product.id}
+            imgPath={product.imgPath}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
     </div>
   );
 }
