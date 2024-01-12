8c68ff29096519f0165e47b47ae8c0dd83390421
João Victor <joaovba2002@alu.ufc.br>
2024-01-12 00:16:40 -0300

    this

--- my-app/src/Product/index.jsx
+++ my-app/src/Product/index.jsx
@@ -1,11 +1,18 @@
 import Style from "./Product.module.css";
 
-const Product = ({ imgPath, name, price }) => {
+const Product = ({ imgPath, name, price, qtd }) => {
+  const isOutOfStock = qtd === 0;
+
   return (
-    <div className={Style.Produto}>
-      <img src={imgPath} />
+    <div className={`${Style.Produto} ${isOutOfStock ? Style.outOfStock : ""}`}>
+      <img src={imgPath} alt={name} />
       <span>{name}</span>
       <span>{price}</span>
+      {isOutOfStock ? (
+        <span className={Style.outOfStockText}>Out of Stock</span>
+      ) : (
+        <button className={Style.buyButton}>Buy</button>
+      )}
     </div>
   );
 };
