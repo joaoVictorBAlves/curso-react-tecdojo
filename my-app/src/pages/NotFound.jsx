be1dde2169a3908b12fa070ab58b135f3370fa4b
João Victor <joaovba2002@alu.ufc.br>
2024-01-24 16:49:29 -0300

    aula-28

--- my-app/src/pages/NotFound.jsx
+++ my-app/src/pages/NotFound.jsx
@@ -1,12 +1,20 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Page not found</h1>
-            <button>
-                <a href="/">Voltar</a>
-            </button>
-        </div>
-    );
-}
- 
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1)
+  };
+
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <p>The {location.pathname} doesn't exist :(</p>
+      <button onClick={handleGoBack}>Voltar</button>
+    </div>
+  );
+};
+
 export default NotFound;
