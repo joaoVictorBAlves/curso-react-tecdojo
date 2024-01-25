9ed9b9a277feded9dd95cee61f8de0486037c4c4
João Victor <joaovba2002@alu.ufc.br>
2024-01-25 15:58:15 -0300

    aula-32

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,30 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import ContactUs from "./pages/ContactUs";
-import Product from "./pages/ProductDetail";
-import NotFound from "./pages/NotFound";
-import "./index.css";
-import ProductDetail from "./pages/ProductDetail";
-import Navbar from "./components/Navbar";
-
 const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="Content">
-          <Routes>
-            <Route path="/" exact Component={Home} />
-            <Route path="/contact" Component={ContactUs} />
-            <Route path="/about" Component={About} />
-            <Route path="/product/:id" Component={ProductDetail} />
-            <Route path="*" Component={NotFound} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
-  );
+  return <div>Persistência de Dados</div>;
 };
 
 export default App;
